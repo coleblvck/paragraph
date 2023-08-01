156a9b09343ef180ea7f6cfe4a96b2daf3ef88bc
childoftext <childoftext@gmail.com>
2023-08-01 23:02:40 +0100

    Register NowPlaying in Admin

--- live_mode/admin.py
+++ live_mode/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 from .models import NowPlaying
 
 # Register your models here.
-admin.site.register(NowPlaying)
+class NowPlayingAdmin(admin.ModelAdmin):
+    list_filter = ['user']
+    list_display = ['user']
+    search_fields = ['user']
+    readonly_fields = ['user']
+
+    class Meta:
+        model = NowPlaying
+admin.site.register(NowPlaying, NowPlayingAdmin)
