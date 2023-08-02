c901b8f5d1560537e4b93f6fa45fa6dbaaaee74f
osuser <childoftext@opal12.opalstack.com>
2023-08-02 16:52:52 +0000

    fix by sean for server breakage

--- paragraph/wsgi.py
+++ paragraph/wsgi.py
@@ -16,7 +16,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'paragraph.settings')
 
 application = get_wsgi_application()
 
-from email_verification.views import delete_inactive_users
-
-t = Thread(target=delete_inactive_users)
-t.start()
+#from email_verification.views import delete_inactive_users
+#
+#t = Thread(target=delete_inactive_users)
+#t.start()
