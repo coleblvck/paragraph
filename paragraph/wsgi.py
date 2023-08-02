a99641351bbac067e86e278f4e9c13f2f4749430
childoftext <childoftext@gmail.com>
2023-08-02 02:48:39 +0100

    setting up email verification

--- paragraph/wsgi.py
+++ paragraph/wsgi.py
@@ -14,9 +14,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'paragraph.settings')
 
 application = get_wsgi_application()
-
-from threading import Thread
-from email_verification.views import delete_inactive_users
-
-t = Thread(target=delete_inactive_users)
-t.start()
