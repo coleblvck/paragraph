0ef28033b977af40e7e355a859a9bf436346eddc
childoftext <childoftext@gmail.com>
2023-08-07 22:05:53 +0100

    fixed readonly field in AccountAdmin

--- account/admin.py
+++ account/admin.py
@@ -8,7 +8,7 @@ from django.apps import apps
 class AccountAdmin(UserAdmin):
     list_display = ('email', 'username', 'date_joined', 'last_login', 'is_admin', 'is_staff')
     search_fields = ('email', 'username')
-    readonly_fields = ('id', 'date_joined', 'last_login')
+    readonly_fields = ('user_id', 'date_joined', 'last_login')
 
     # Set due to errors requesting for filters
     filter_horizontal = ()
