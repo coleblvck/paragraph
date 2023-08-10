29cb17da4b36d2f3304e1568e3293d2f4e0caad1
childoftext <childoftext@gmail.com>
2023-08-10 12:02:48 +0100

    An attempt to fix profile actions query

--- paragraph/schema.py
+++ paragraph/schema.py
@@ -189,7 +189,7 @@ class Query(graphene.ObjectType):
             seentimeadjusted = seentime.strftime("%c")
             return seentimeadjusted
         
-    profileactions = graphene.List(user_id= graphene.Int())
+    profileactions = graphene.Field(user_id= graphene.Int())
     def resolve_profileactions(root, info, user_id):
         profile_relation = {}
         profile_relation["button1"] = ""
