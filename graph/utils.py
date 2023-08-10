cd41ffaafdbd8c8b25de995b32854b9d667bb2f6
childoftext <childoftext@gmail.com>
2023-08-10 23:46:46 +0100

    Setting up push notifications

--- graph/utils.py
+++ graph/utils.py
@@ -19,7 +19,6 @@ def send_message(user, title, body):
 def send_test_message():
     message = Message(
         notification=Notification(title="This works", body="Thank God!!!!!!!!!!!!", image=""),
-        topic="Notification from paragraph",
         token="cuiMZ8A5TPGjheZrGOW5L4:APA91bG5CAmkhiG0jVBtuxQQq1PcmReuV8lJiJwq-TyceqIh8Hanw33YyD0-WUtxL9wfrJpYrcSPypdE4VQtTCPxQQpOvUTZ4V-TIjVMvYuzLIaGPtvaMYx3o-mLreIwtH1n2dVzdNKt",
         )
     response = send(message)
