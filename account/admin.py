e2740fd5e1e3dcb68246b5a08f9810e394e22126
childoftext <childoftext@gmail.com>
2023-08-10 09:24:08 +0100

    removed graphql-auth from admin.py in account

--- account/admin.py
+++ account/admin.py
@@ -17,8 +17,3 @@ class AccountAdmin(UserAdmin):
 
 
 admin.site.register(Account, AccountAdmin)
-
-app = apps.get_app_config('graphql_auth')
-
-for model_name, model in app.models.items():
-    admin.site.register(model)
