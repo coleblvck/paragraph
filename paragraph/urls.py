fc9b7175d212d572b290c78ce40c9312a7037c2d
childoftext <childoftext@gmail.com>
2023-08-14 06:42:33 +0100

    Removed all old paths

--- paragraph/urls.py
+++ paragraph/urls.py
@@ -27,23 +27,9 @@ from django.views.decorators.csrf import csrf_exempt
 from graphene_file_upload.django import FileUploadGraphQLView
 
 urlpatterns = [
-    path('', index, name="home"),
     path('paragraph/admin', admin.site.urls),
-    path('chat/<friend>', chat, name="chat"),
-    path('texts', textsview, name="textsview"),
-    path('get-started', registerview, name="register"),
-    path('logout', logoutview, name="logoutview"),
     path('login', loginview, name="loginview"),
     # API Views
-    path('sent_msg/<friend>', sentmessages, name="sent_msg"),
-    path('received_msg/<friend>', receivedmessages, name="received_msg"),
-    path('textsscreen', textsscreen, name="textsscreen"),
-    #
-    path('user/', include('account.urls', namespace="user")),
-    path('update', profileupdate, name="update"),
-    path('dashboard', dashboardview, name="dashboardview"),
-    path('userquery', userquery, name="userquery"),
-    path('search/<username>/', usersearch, name="usersearch"),
 
     # Graphql
     path('graphql', csrf_exempt(FileUploadGraphQLView.as_view(graphiql=True))),
